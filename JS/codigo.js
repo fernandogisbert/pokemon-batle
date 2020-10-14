2acf2fac2060b91e4dfe0178e91cfe9df80394b1
LAPTOP-1U1SBC38\ferna <ferbert15@gmail.com>
2020-10-14 08:49:59 +0200

    14-10-2020

--- JS/codigo.js
+++ JS/codigo.js
@@ -3,24 +3,24 @@
 const botonLuchar = document.getElementById('boton__luchar');
 
 
+var vidaEsquirtel;
+var vidaBulbasur;
 
-function luchar() {
 
+
+function lucha (){
 
     // parámetros esquirtel
-    var vidaEsquirtel = document.getElementById('vida__esquirtel').value;
+    vidaEsquirtel = document.getElementById('vida__esquirtel').value;
     let ataqueEsquirtel = document.getElementById('ataque__esquirtel').value;
     let defensaEsquirtel = document.getElementById('defensa__esquirtel').value;
 
     // parámetros bulbasur
 
-    var vidaBulbasur = document.getElementById('vida__bulbasur').value;
+    vidaBulbasur = document.getElementById('vida__bulbasur').value;
     let ataqueBulbasur = document.getElementById('ataque__bulbasur').value;
     let defensaBulbasur = document.getElementById('defensa__bulbasur').value;
 
-    // vencedor
-
-    let vencedor = '';
 
     // parámetros de lucha 
 
@@ -33,61 +33,77 @@ function luchar() {
     }
 
     let aMeter = "";
-    
-    
-    while( (vidaBulbasur && vidaEsquirtel) > 0) {
 
-        // setInterval(function(){ 
-            player1();
-            
-            player2();
-
-            let paraIntroducir = document.getElementById('battle__transcurso');
-            
-
-            aMeter+= `<p>Squirtel ataca a Bulbasur y lo deja con ${vidaBulbasur} de vida</p>
-            <p>Bulbasur ataca a Squirtel y lo deja con ${vidaEsquirtel} de vida</p>`;
-
-            paraIntroducir.innerHTML = aMeter; 
-            
-
-            console.log(aMeter);
-
-            // aquí ocurre la lucha
-            console.log(vidaEsquirtel, vidaBulbasur);
-    //     }, 3000);
-    }
-
-    //  frase sale final segun quien gana
-    //  caracteristicas imagen del ganador 
-    
-    esquirtel = document.getElementById('squirtelId');
-    bulbasur = document.getElementById('bulsaurId');
-
-    if (vidaEsquirtel > 0){
-        vencedor = vencedor + 'El ganador es Squirtel';
-        bulbasur.classList.remove('ganador__tamano');
-        esquirtel.classList.add('ganador__tamano');
-    }else{
-        vencedor = vencedor + 'El ganador es bulbasur';
-        esquirtel.classList.remove('ganador__tamano');
-        bulbasur.classList.add('ganador__tamano');
-    }
-
-    
-
-    let resultado = document.getElementById('battle__result');
-
-    let aInsertar = `
-        <h3>${vencedor}</h3>
-        `;
-
-    resultado.innerHTML = aInsertar;
-    
     let ring = document.getElementById('battle__ring');
     ring.classList.add('fondoNaranja');
 
+    // aquí ocurre la lucha
+
+    var luchaIntervalo = setInterval(function(){ 
+
+                if( (vidaBulbasur && vidaEsquirtel) > 0){
+
+                    player1();
+                    
+                    player2();
+
+                    let paraIntroducir = document.getElementById('battle__transcurso');
+                    
+
+                    aMeter+= `<p>Squirtel ataca a Bulbasur y lo deja con ${vidaBulbasur} de vida</p>
+                    <p>Bulbasur ataca a Squirtel y lo deja con ${vidaEsquirtel} de vida</p>`;
+
+                    paraIntroducir.innerHTML = aMeter; 
+                    
+
+                    console.log(aMeter);
+                    console.log(vidaEsquirtel, vidaBulbasur);
+
+                    
+                }else{
+                    
+                    clearInterval(luchaIntervalo);
+
+                    esquirtel = document.getElementById('squirtelId');
+                    bulbasur = document.getElementById('bulsaurId');
+
+                    if (vidaEsquirtel > 0){
+                        vencedor = vencedor + 'El ganador es Squirtel';
+                        bulbasur.classList.remove('ganador__tamano');
+                        esquirtel.classList.add('ganador__tamano');
+                    }else{
+                        vencedor = vencedor + 'El ganador es bulbasur';
+                        esquirtel.classList.remove('ganador__tamano');
+                        bulbasur.classList.add('ganador__tamano');
+                    }
+
+                    let resultado = document.getElementById('battle__result');
+
+                    let aInsertar = `
+                        <h3>${vencedor}</h3>
+                        `;
+
+                    resultado.innerHTML = aInsertar;
+                    
+                    let ring = document.getElementById('battle__ring');
+                    ring.classList.add('fondoNaranja');
+                        
+                    }
+        }, 500);
 
 }
 
-botonLuchar.addEventListener('click', luchar());
+// vencedor
+let vencedor = '';
+
+
+
+botonLuchar.addEventListener('click', () => {
+    lucha();
+  });
+
+botonLuchar.addEventListener('click', () => {
+
+    localStorage.setItem("nombre", "Horse-Luis");
+    sessionStorage.setItem("perico", "el de los palotes");
+})
